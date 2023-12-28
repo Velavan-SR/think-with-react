32ffc676aff2ec5bc93c57d51d23a8d5266fc35b
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-12-28 17:32:42 +0530

    Done

---
+++ part1/app.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('react-container');
+
+ReactDOM.render("Hello! Welcome to React",container);
+
+    // Functional component
+
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
+
+    // Render the Container
+
+ReactDOM.render(React.createElement(Container),container);
+
+    // Class Component
+
+class ReactContainer extends React.Component{
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  ReactDOM.render(React.createElement(ReactContainer),container);
